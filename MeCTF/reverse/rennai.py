eb355cd0ad3110fec31a67525678770870710073
User <user@Jamess-MacBook-Air.local>
2018-11-14 23:51:31 +0800

    add explanation

--- MeCTF/reverse/rennai.py
+++ MeCTF/reverse/rennai.py
@@ -1,10 +1,13 @@
+###code obfuscation problem###
+######tricks of meddling IDApro output
+########useless ret
+########function with name similar to libc functions
+
+
 import binascii
 
-
-
-
 cle = (313373133731337//31337)*3
-cle = cle^0x4a8255542^0x54016200
+cle = cle^0x4a8255542
 cne = 1
 v1 = 4196686
 DATA = 'CC 08 0C 00 02 00 00 00  95 08 0C 00 02 00 00 00 \
@@ -26,11 +29,12 @@ CA 08 0C 00 02 00 00 00  91 08 0C 00 02 00 00 00 \
 91 08 0C 00 02 00 00 00  91 08 0C 00 02 00 00 00 \
 91 08 0C 00 02 00 00 00'
 DATA = ''.join(DATA.split(' ')[::-1]).replace('000000',' ').split(' ')[::-1][:-1]
+v3 = cle^0x54016200
 v4 = []
 for i in DATA:
     v4.append(int(i,16))
 print('CTFS{',end = '')
 for i in range(28):
-    print(chr(cle^v4[i]),end='')
+    print(chr(v3^v4[i]),end='')
 
 print('}')
