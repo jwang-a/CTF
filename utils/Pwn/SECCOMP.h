de5e0ee74f833d32d620222b0f632ef798235f6f
jwang-a <b05902008@ntu.edu.tw>
2020-03-25 15:27:35 +0800

    seccomp macro header

---
+++ utils/Pwn/SECCOMP.h
@@ -0,0 +1,19 @@
+/*Some seccomp constructor macros for pwn problems*/
+
+#include<stddef.h>
+#include<sys/prctl.h>
+#include<linux/filter.h>
+#include<linux/seccomp.h>
+#include<linux/audit.h>
+#include<linux/unistd.h>
+
+#define ArchField offsetof(struct seccomp_data, arch)
+#define SyscallNum offsetof(struct seccomp_data, nr)
+
+#define Allow(syscall) \
+  BPF_JUMP(BPF_JMP | BPF_JEQ | BPF_K, __NR_##syscall, 0, 1),\
+  BPF_STMT(BPF_RET | BPF_K, SECCOMP_RET_ALLOW)
+
+#define Disallow(syscall) \
+  BPF_JUMP(BPF_JMP | BPF_JEQ | BPF_K, __NR_##syscall, 0, 1),\
+  BPF_STMT(BPF_RET | BPF_K, SECCOMP_RET_KILL)
