978897eea15e1eafb053ed7a524e1812263eef7e
jwang-a <b05902008@ntu.edu.tw>
2024-01-31 19:00:42 -0800

    cleanup Cobolstrike unused files

--- MyChallenges/Pwn/Cobolstrike/sourcecode/build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-docker build . -t cobolstrike-build
-docker run -v ./:/root/:rw --rm cobolstrike-build
-docker rmi cobolstrike-build
--- MyChallenges/Pwn/Cobolstrike/sourcecode/compile.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-cobc -x /root/cobolstrike.cbl -o /root/cobolstrike
